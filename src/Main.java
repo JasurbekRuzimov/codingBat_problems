d06f77c0ae4b67edfd1d2604f18eebb97f39bc56
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-01-30 10:38:07 +0500

    main class added

---
+++ src/Main.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Biror son kiriting");
+        n = scanner.nextInt();
+        String binary = Integer.toBinaryString(n);
+        System.out.println("  " + n + " Ikkilik sanoq sistemasiga o'tganda  🔜  " + binary + "   shunday ko'rinishda bo'ladi");
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
