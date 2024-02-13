90a3e3c676070b2fbe8ba73634ff055cacb0a7d3
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-02-13 11:03:01 +0500

    update git

--- src/uz/pdp/CodingBatProblems/cpython/TublikkaTekshirish.java
+++ src/uz/pdp/CodingBatProblems/cpython/TublikkaTekshirish.java
@@ -1,2 +1,23 @@
-package uz.pdp.CodingBatProblems.cpython;public class TublikkaTekshirish {
+package uz.pdp.CodingBatProblems.cpython;
+
+import java.util.Scanner;
+
+public class TublikkaTekshirish {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = i + 1; j <= n; j++) {
+        if (i % j !=0) {
+
+        }
+
+      }
+    }
+
+  }
+
 }
